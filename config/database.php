beaded3b12ca30ee038ed330bf4210b261c0eddf
Daniel Medeiros <danielmmf@gmail.com>
2021-07-27 20:04:31 -0300

    mysql

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '172.16.79.154'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'eiti_adega'),
+            'username' => env('DB_USERNAME', 'eiti_eiti'),
+            'password' => env('DB_PASSWORD', 'pW61MAVPDEfyLqOb2ni6J9ly0VVHF0ib'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
